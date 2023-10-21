758d187481a5ba4d32c8bce556620e8383c96b18
zhongjiahui <zhongjiahui@authing.cn>
2023-10-21 13:01:12 +0800

    init router lib

---
+++ lib_router_annotation/src/main/java/com/zjh/router/annotation/bean/RouterBean.java
@@ -0,0 +1,18 @@
+package com.zjh.router.annotation.bean;
+
+/**
+ * @author Administrator
+ * @date 2023/10/21
+ * @description
+ **/
+public class RouterBean {
+
+    public enum TypeEnum{
+        ACTIVITY,
+        CALL
+    }
+
+
+
+
+}
